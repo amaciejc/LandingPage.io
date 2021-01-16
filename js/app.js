5791eed39999b6dcfc5f32f2322b4ffe3ea2d33a
amaciejc <75544555+amaciejc@users.noreply.github.com>
2021-01-16 20:54:33 +0100

    First Commit before refactoring

--- js/app.js
+++ js/app.js
@@ -1,22 +1,14 @@
-/**
- * 
- * Manipulating the DOM exercise.
- * Exercise programmatically builds navigation,
- * scrolls to anchors from navigation,
- * and highlights section in viewport upon scrolling.
- * 
- * Dependencies: None
- * 
- * JS Version: ES2015/ES6
- * 
- * JS Standard: ESlint
- * 
+
+/*
+ * Define Global Variables
 */
 
-/**
- * Define Global Variables
- * 
-*/
+//AM: the UL that will keep the menu is placed in HTML (id="navbar__list").
+const navList = document.querySelector('#navbar__list');
+// AM: Constant for storing a list of all sections. Build when loading the page
+const sections = document.querySelectorAll('section');
+let activeSection = 0
+const navItems = navList.children;
 
 
 /**
@@ -25,7 +17,28 @@
  * 
 */
 
+//Determines which section is in viewport amd applying style to active elements
 
+document.addEventListener("scroll", ()=>{
+    for (let el of sections){
+        let bounding = el.getBoundingClientRect();
+        if(bounding.top <300 & bounding.top >-300
+        ){
+            el.classList.add("active__class");
+            activeSection = el;
+        }
+        else {el.classList.remove("active__class")}
+    }
+    return activeSection
+})
+
+// for (let el of navItems){
+//     if(el.getAttribute("id")===activeSection.getAttribute("id")){
+//         el.classList.add("active_nav")}
+//         else {
+//             el.classList.remove("active_nav")
+//         }
+// }
 
 /**
  * End Helper Functions
@@ -33,14 +46,27 @@
  * 
 */
 
-// build the nav
+// function building the nav
+
+const navBuilder = function(){
+for (let i=0; i<sections.length; i++) {
+    const navItem = document.createElement('li');
+    navItem.textContent = sections[i].dataset.nav;
+    navList.appendChild(navItem);
+    navItem.style.cssText='border: 1px solid; color: black; padding: 5px; margin: 5px; background-color: yellow;';
+    navItem.addEventListener("click", e=>{
+        const href = document.querySelector("#"+sections[i].getAttribute("id"));
+        href.scrollIntoView({behavior: "smooth"});
+        
+    })
+}}
 
 
+
+document.addEventListener('DOMContentLoaded',navBuilder)
 // Add class 'active' to section when near top of viewport
 
-
 // Scroll to anchor ID using scrollTO event
-
 
 /**
  * End Main Functions
@@ -54,4 +80,4 @@
 
 // Set sections as active
 
-
+//TO DO
