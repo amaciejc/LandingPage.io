6e14637ce31a884a18ed904f3b8550c7934d55a1
amaciejc <75544555+amaciejc@users.noreply.github.com>
2021-01-24 16:28:54 +0100

    - removed the additional active class (the dotted border) - removed unnecessary variables like navItems, activeSection - replaced the way item on the nav is built from .innerText to .innerHTML and added the <a> attribute within (there was none earlier, just the) - created a variable inside the for of sections (activeLink) that gets the navlist item corresponding to active class and gives it an active class - removed the css classes added earlier - the starter pack had it all - removed unnecessary and commmented out text

--- js/app.js
+++ js/app.js
@@ -2,43 +2,35 @@
 /*
  * Define Global Variables
 */
-
-//AM: the UL that will keep the menu is placed in HTML (id="navbar__list").
+//AM: the UL that will keep the menu taken from HTML
 const navList = document.querySelector('#navbar__list');
-// AM: Constant for storing a list of all sections. Build when loading the page
+//AM: Constant for storing a list of all sections. Build when loading the page
 const sections = document.querySelectorAll('section');
-let activeSection = 0
-const navItems = navList.children;
 
-
-/**
+/*
  * End Global Variables
  * Start Helper Functions
  * 
 */
 
-//Determines which section is in viewport amd applying style to active elements
+//Determines which section is in viewport amd applies style to active elements
 
 document.addEventListener("scroll", ()=>{
     for (let el of sections){
         let bounding = el.getBoundingClientRect();
+        let activeLink = document.querySelector(`[data-link="${el.dataset.nav}"]`)//this gets the navigation li that corresponds to the active section based on the dataset attributes//
         if(bounding.top <300 & bounding.top >-300
         ){
-            el.classList.add("active__class");
-            activeSection = el;
+            el.classList.add("active__class");//style for section
+            activeLink.classList.add("active_nav")//style for nav bar item
         }
-        else {el.classList.remove("active__class")}
+        else {
+            el.classList.remove("active__class");
+            activeLink.classList.remove("active_nav")
+        }
     }
-    return activeSection
-})
 
-// for (let el of navItems){
-//     if(el.getAttribute("id")===activeSection.getAttribute("id")){
-//         el.classList.add("active_nav")}
-//         else {
-//             el.classList.remove("active_nav")
-//         }
-// }
+})
 
 /**
  * End Helper Functions
@@ -51,19 +43,17 @@ document.addEventListener("scroll", ()=>{
 const navBuilder = function(){
 for (let i=0; i<sections.length; i++) {
     const navItem = document.createElement('li');
-    navItem.textContent = sections[i].dataset.nav;
+    navItem.innerHTML =`<a href="#${sections[i].id}" class="menu__link" data-link="${sections[i].dataset.nav}"> ${sections[i].dataset.nav}</a>`;
     navList.appendChild(navItem);
-    navItem.style.cssText='border: 1px solid; color: black; padding: 5px; margin: 5px; background-color: yellow;';
     navItem.addEventListener("click", e=>{
         const href = document.querySelector("#"+sections[i].getAttribute("id"));
-        href.scrollIntoView({behavior: "smooth"});
-        
+        href.scrollIntoView({behavior: "smooth"});s
     })
+
 }}
 
-
-
 document.addEventListener('DOMContentLoaded',navBuilder)
+
 // Add class 'active' to section when near top of viewport
 
 // Scroll to anchor ID using scrollTO event
@@ -81,3 +71,11 @@ document.addEventListener('DOMContentLoaded',navBuilder)
 // Set sections as active
 
 //TO DO
+/* 24.01
+- removed the additional active class (the dotted border)
+- removed unnecessary variables like navItems, activeSection
+- replaced the way item on the nav is built from .innerText to .innerHTML and added the <a> attribute within (there was none earlier, just the)
+- created a variable inside the for of sections (activeLink) that gets the navlist item corresponding to active class and gives it an active class
+- removed the css classes added earlier - the starter pack had it all
+- removed unnecessary and commmented out text
+*/
